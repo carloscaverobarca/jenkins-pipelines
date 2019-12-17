dd9d616ec136023df8be560968fd7441bb7d8729
Cavero Barca <carlos.cavero@atos.net>
2019-12-17 12:20:52 +0100

    Add pipeline GitLab Merge Request example

---
+++ create-pipeline-dsl/README.md
@@ -0,0 +1,24 @@
+# Description
+
+This pipeline creates a Jenkins GitLab Pipeline for Merge Requests job using Groovy Domain-Specific Language (DSL) under a specific folder
+
+# Pre-requisites
+
+- [Job DSL plugin](https://wiki.jenkins.io/display/JENKINS/Job+DSL+Plugin)
+
+# Instructions
+
+- Create a new clean `Freestyle project` job 
+- Select `Process Job DSLs` in the `Build` section
+- Select `Use the provided DSL script` 
+- Copy the content to `DSL script` textbox modifying the `#` variables
+- It is also possible to clone this repo, select `Look on Filesystem` and include the path to the `groovy` file
+
+# External resources
+
+- [Jenkins Job DSL API Viewer](https://jenkinsci.github.io/job-dsl-plugin/)
+- [pipelineJob documentation](https://jenkinsci.github.io/job-dsl-plugin/#path/pipelineJob)
+
+# License
+
+Apache 2.0
---
+++ create-pipeline-dsl/pipeline-merge-request-dsl.groovy
@@ -0,0 +1,61 @@
+/**
+ * Copyright (C) 2019 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @author 	Carlos Cavero
+ *
+ * It creates a Jenkins GitLab Pipeline job using Groovy Domain-Specific Language (DSL)
+ * under a specific folder. It accepts Merge Requests to develop branch
+ */
+
+[
+  '#reponame'
+].each { service ->
+  
+  def lab = '#lab'
+
+  pipelineJob("${lab}/${service}-pipeline-build") {
+
+    description('Test description')
+    
+    displayName(service + "-pipeline-build")
+
+    triggers {
+        gitlabPush {
+            buildOnMergeRequestEvents(true)
+            buildOnPushEvents(false)
+            enableCiSkip(true)
+            setBuildDescription(true)
+            rebuildOpenMergeRequest('source')
+            includeBranches('develop')
+			excludeBranches('master')
+        }
+    }
+	
+    definition {
+        cpsScm {
+			lightweight(true)
+			scm {
+				git {
+					remote {
+						url("#repogit")
+						credentials('#credentials')
+					}
+					branches('*/*')
+				}
+			}
+		}
+    }
+  }
+}
